4b9fa6efed34a318ac2f1aa68e39ba44ac0c4246
Eddy <edifonemmanuel14@gmail.com>
2025-04-18 22:03:28 +0100

    updated file

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ urine_mapper = {"NEGATIVE":0, "POSITIVE":1, "GLUCOSE TRACE":2, "PROTEIN TRACE":3
 
 st.title("URINE ANALYSIS (Proteinuria)")
 
-st.markdown("Provide values for the 10 clinical features below:")
-
 maternal_age = st.number_input("Maternal Age", min_value=15, max_value=50, step=1, value=29)
 
 gestational_age = st.number_input("Gestational Age (weeks)", min_value=3.0, max_value=42.0, step=0.5, value=32.0)
